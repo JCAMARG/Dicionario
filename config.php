3551fe52a9a3bca5ae7935a209d44f7c48a01925
Joao Antonio de Camargo <33570045+JCAMARG@users.noreply.github.com>
2025-04-24 15:41:47 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,11 @@
 <?php
 	define("SITE_SUBFOLDER","");
 	define("SITE_ROOT", $_SERVER["DOCUMENT_ROOT"].SITE_SUBFOLDER);
-	define("SITE_URL", "http://". $_SERVER["SERVER_NAME"].SITE_SUBFOLDER);
+	//define("SITE_URL", "http://". $_SERVER["SERVER_NAME"].SITE_SUBFOLDER);
+
+	$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
+	$host = $_SERVER['HTTP_HOST'] ?? $_SERVER['SERVER_NAME']; // fallback se HTTP_HOST não existir
+	define("SITE_URL", $protocol . "://" . $host . SITE_SUBFOLDER);
 	
 	define("_DB_HOST", "localhost");
 	define("_DB_USER", "root");
