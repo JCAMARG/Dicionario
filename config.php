8d87b765f2af3530d338742c44649584c2fc4c9b
Joao Antonio de Camargo <33570045+JCAMARG@users.noreply.github.com>
2025-04-24 15:43:02 -0300

    Update config.php

--- config.php
+++ config.php
@@ -3,9 +3,13 @@
 	define("SITE_ROOT", $_SERVER["DOCUMENT_ROOT"].SITE_SUBFOLDER);
 	//define("SITE_URL", "http://". $_SERVER["SERVER_NAME"].SITE_SUBFOLDER);
 
-	$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
-	$host = $_SERVER['HTTP_HOST'] ?? $_SERVER['SERVER_NAME']; // fallback se HTTP_HOST não existir
-	define("SITE_URL", $protocol . "://" . $host . SITE_SUBFOLDER);
+	if ($_SERVER['SERVER_NAME'] === '0.0.0.0') {
+		define("SITE_URL", "https://dicionario-php.onrender.com");
+	} else {
+		$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
+		$host = $_SERVER['HTTP_HOST'] ?? $_SERVER['SERVER_NAME'];
+		define("SITE_URL", $protocol . "://" . $host . SITE_SUBFOLDER);
+	}
 	
 	define("_DB_HOST", "localhost");
 	define("_DB_USER", "root");
