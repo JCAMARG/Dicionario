38467757c9564c0717268ad88b1033d135c26322
Joao Antonio de Camargo <33570045+JCAMARG@users.noreply.github.com>
2025-04-25 10:15:15 -0300

    Create recriar_tabelas.php

---
+++ recriar_tabelas.php
@@ -0,0 +1,25 @@
+<?php
+include("./config.php");
+include(constant("SITE_ROOT")."/funcoes.php"); // ou onde está sua classe mysql
+
+$db = new mysql();
+
+$acao = $_GET['acao'] ?? '';
+
+switch ($acao) {
+    case 'disciplinas':
+        $db->resetDisciplinaTable();
+        echo "Tabela 'disciplinas' recriada com sucesso!";
+        break;
+
+    case 'dicionario':
+        $db->resetDicionarioTable();
+        echo "Tabela 'dicionario' recriada com sucesso!";
+        break;
+
+    default:
+        echo "Ação inválida.";
+        break;
+}
+?>
+<br><a href="index.php">Voltar para a Home</a>
