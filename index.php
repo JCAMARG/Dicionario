88461b5fa6b6ebbcf1e703b4fc228bcc3a321e62
Joao Antonio de Camargo <33570045+JCAMARG@users.noreply.github.com>
2025-04-25 11:19:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,11 @@ include(constant("SITE_ROOT")."/header.php");
         <form action="recriar_tabelas.php" method="GET"  style="margin: 0">
             <select name="acao" style="padding: 5px; font-size: 14px;">
                 <option value="">Selecione uma Tabela</option>
-                <option value="disciplinas">Recriar Tabela Disciplinas</option>
-                <option value="dicionario">Recriar Tabela Dicionário</option>
+                <option value="criar_disciplinas">Recriar Tabela Disciplinas</option>
+                <option value="criar_dicionario">Recriar Tabela Dicionário</option>
+                <option value="ins_disc">Carregar dados Disciplinas</option>
+                <option value="ins_dici">Carregar dados Dicionário</option>
+                <option value="restaurar">Restaurar banco completo</option>
             </select>
             <button type="submit" style="padding: 6px 10px; background-color: #007BFF; color: white; border: none; border-radius: 5px; cursor: pointer;">Recriar</button>
         </form>
