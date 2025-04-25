d00a0de480de7ae5fb2e34494a9190f88f178548
Joao Antonio de Camargo <33570045+JCAMARG@users.noreply.github.com>
2025-04-25 13:05:11 -0300

    Update editar.php

--- dicionario/editar.php
+++ dicionario/editar.php
@@ -84,18 +84,18 @@ if (isset($erro)) {
 	<table class="lista" style="border:1px solid slategrey; border-style:outset;">
 		<tr>
 			<td>Palavra:</td>
-			<td style="width: 100%;"><input type="text" name="palavra_orig" style="padding: 3px; border:1px solid grey; border-style:inset;" value="<?=isset($palavra_orig)?$palavra_orig:"";?>"></td>
+			<td><input type="text" name="palavra_orig" style="padding: 3px; border:1px solid grey; border-style:inset;" value="<?=isset($palavra_orig)?$palavra_orig:"";?>" style="width: 100%;"></td>
 		</tr>
 
 		<tr>
 			<td>Significado:</td>
-			<td style="width: 100%;"><input type="text" name="significado" style="padding: 3px; border:1px solid grey; border-style:inset;" value="<?=isset($significado)?$significado:"";?>"></td>
+			<td><input type="text" name="significado" style="padding: 3px; border:1px solid grey; border-style:inset;" value="<?=isset($significado)?$significado:"";?>" style="width: 100%;"></td>
 		</tr>
 		
 		<tr>
 			<td>Disciplinas:</td>
-			<td style="width: 100%;">
-				<select style="width:179px; padding:3px" name="id_disciplina">
+			<td>
+				<select style="width:179px; padding:3px" name="id_disciplina" style="width: 100%;">
 		                    <?php
 							echo "<option></option>";
 		                        while ($row = pg_fetch_assoc($resultDis)) {
