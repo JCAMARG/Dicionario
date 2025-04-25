c882411ac5781a0d889fef376853894dee364000
Joao Antonio de Camargo <33570045+JCAMARG@users.noreply.github.com>
2025-04-25 11:48:02 -0300

    Update recriar_tabelas.php

--- recriar_tabelas.php
+++ recriar_tabelas.php
@@ -9,38 +9,44 @@ switch ($acao) {
     case 'criar_disciplinas':
         $db->dropTable('dicionario');
         $db->createDicionarioTable();
+    
         $db->dropTable('disciplinas');
         $db->createDisciplinaTable();
+    
         echo "Tabela 'disciplinas' recriada com sucesso!";
         break;
 
     case 'criar_dicionario':
         $db->dropTable('dicionario');
         $db->createDicionarioTable();
+    
         echo "Tabela 'dicionario' recriada com sucesso!";
         break;
 
     case 'ins_disc':
         // Carregar dados na tabela 'disciplinas'
         $db->insertDisciplinaData();
+    
         echo "Dados carregados na tabela 'disciplinas' com sucesso!";
         break;
 
     case 'ins_dici':
         // Carregar dados na tabela 'dicionario'
         $db->insertDicionarioData();
+    
         echo "Dados carregados na tabela 'dicionario' com sucesso!";
         break;
 
     case 'restaurar':
         // Restaurar as tabelas (dropar e recriar)
-        $db->dropTable('disciplinas');
-        $db->createDisciplinaTable();
-        $db->insertDisciplinaData();
-        
         $db->dropTable('dicionario');
         $db->createDicionarioTable();
         $db->insertDicionarioData();
+    
+        $db->dropTable('disciplinas');
+        $db->createDisciplinaTable();
+        $db->insertDisciplinaData();
+       
 
         echo "Banco restaurado com sucesso!";
         break;
