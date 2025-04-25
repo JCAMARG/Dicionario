c99ab5132a7b5b7e8d8527e1b3561ec00e309178
Joao Antonio de Camargo <33570045+JCAMARG@users.noreply.github.com>
2025-04-25 11:21:15 -0300

    Update recriar_tabelas.php

--- recriar_tabelas.php
+++ recriar_tabelas.php
@@ -6,14 +6,41 @@ $db = new mysql();
 $acao = $_GET['acao'] ?? '';
 
 switch ($acao) {
-    case 'disciplinas':
-        $db->resetDisciplinaTable();
+    case 'criar_disciplinas':
+        $db->dropTable('disciplinas');
+        $db->createDisciplinaTable();
         echo "Tabela 'disciplinas' recriada com sucesso!";
         break;
 
-    case 'dicionario':
-        $db->resetDicionarioTable();
+    case 'criar_dicionario':
+        $db->dropTable('dicionario');
+        $db->createDicionarioTable();
         echo "Tabela 'dicionario' recriada com sucesso!";
+        break;
+
+    case 'ins_disc':
+        // Carregar dados na tabela 'disciplinas'
+        $db->insertDisciplinaData();
+        echo "Dados carregados na tabela 'disciplinas' com sucesso!";
+        break;
+
+    case 'ins_dici':
+        // Carregar dados na tabela 'dicionario'
+        $db->insertDicionarioData();
+        echo "Dados carregados na tabela 'dicionario' com sucesso!";
+        break;
+
+    case 'restaurar':
+        // Restaurar as tabelas (dropar e recriar)
+        $db->dropTable('disciplinas');
+        $db->createDisciplinaTable();
+        $db->insertDisciplinaData();
+        
+        $db->dropTable('dicionario');
+        $db->createDicionarioTable();
+        $db->insertDicionarioData();
+
+        echo "Banco restaurado com sucesso!";
         break;
 
     default:
